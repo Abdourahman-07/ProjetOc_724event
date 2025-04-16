da4ac521d3d5c2fe8595126bef89ec0325f0aab0
Abdourahman Laloue <abdourahman.laloue@outlook.fr>
2025-04-16 11:54:15 +0200

    correction bug : affichage de la dernière prestation dans le footer

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,6 +19,12 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+
+  const getLast = () =>
+    data.events.sort(
+      (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)
+    )[0];
+
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
@@ -30,13 +36,14 @@ export const DataProvider = ({ children }) => {
     if (data) return;
     getData();
   });
-  
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        last: getLast(),
       }}
     >
       {children}
@@ -46,7 +53,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
